eab848d810b0cff749b34a95cd2e4a7c1d0ba5dd
Two00434 <two00434@hotmail.com>
2023-10-09 19:59:58 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ set -eu
 
 # $RANDOM returns a different random integer at each invocation.
 # Nominal range: 0 - 32767 (signed 16-bit integer).
-echo "running here"
+echo "running here----done"
 # This script will succeed (exit 0) 50% of the time
-[[ $RANDOM -gt 16384 ]]
+#[[ $RANDOM -gt 16384 ]]
